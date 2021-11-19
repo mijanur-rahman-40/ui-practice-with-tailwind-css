9200b3c7dabb83426858bd15f04ac74e55b839ff
mijanur-rahman-40 <mijanurrahman31416@gmail.com>
2021-11-19 18:22:11 +0600

    updated

--- component-design-practices/tailwind.config.js
+++ component-design-practices/tailwind.config.js
@@ -1,12 +1,15 @@
 module.exports = {
   purge: [],
+  // mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
     extend: {
-      
+      backgroundColor: ['group-hover', 'focus-within', 'hover', 'focus', 'active'],
+      fontSize: ['hover'],
+      translate: ['hover', 'active'],
     },
   },
   plugins: [],
